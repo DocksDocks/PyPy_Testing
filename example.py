8ca93c87fe1f4bf9853f3ddd98f8c3c6200855dc
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2023-01-02 14:36:40 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 import time
 
 t1 = time.time()
-nums = range(1000000)
+nums = range(10000000)
 sum = 0
 for k in nums:
     sum = sum + k
-print("Sum of 100,000,000 numbers is : ", sum)
+print(f"Sum of {len(nums)} numbers is : {sum}")
 t2 = time.time()
 t = t2 - t1
 print("Elapsed time is : ", t, " seconds")
